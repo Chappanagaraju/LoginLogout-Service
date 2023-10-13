5265795d820d613e7a8e19cbaed2f5512fc832ac
Dev2 <Dev2@gamil.com>
2023-10-13 08:54:10 +0530

    loginLogout commit

---
+++ src/main/java/com/springboot/LoginLogoutService/LoginLogoutServiceApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.LoginLogoutService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginLogoutServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginLogoutServiceApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/springboot/LoginLogoutService/controller/LoginController.java
@@ -0,0 +1,38 @@
+package com.springboot.LoginLogoutService.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.springboot.LoginLogoutService.service.LogingService;
+
+@Controller
+public class LoginController {
+
+	@Autowired
+	LogingService service;
+	@RequestMapping(value="/login", method=RequestMethod.GET)
+	public String viewLoginPage(ModelMap model){
+		return "login";
+		
+	}
+	@RequestMapping(value="/login", method=RequestMethod.POST)
+	public String showWelcomePage(ModelMap model, @RequestParam String name, @RequestParam String password) {
+		boolean isValidUser=service.validateUser(name, password);
+		if(!isValidUser) {
+			model.put("errorMessage", "Access Denied, Invalid Credentials");
+			return "login";		
+			}
+		model.put("name", name);
+		model.put("password", password);
+		return "welcome";
+		
+	}
+	@RequestMapping(value="/logout", method=RequestMethod.GET)
+	public String showLogoutPage(ModelMap model) {
+		return "login";
+	}
+}
